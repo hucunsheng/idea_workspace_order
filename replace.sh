bfeaa0685a15fa6e917e62611e43e720433f3b69
hucs <303872867@qq.com>
2017-10-29 01:11:27 +0800

    test

--- replace.sh
+++ replace.sh
@@ -1,10 +1,10 @@
 jar_name=order-0.0.1-SNAPSHOT.jar
 #!编译好的jar包存放地址
-file_path=~/.jenkins/workspace/idea_workspace_order/target
+file_path=/root/.jenkins/workspace/idea_workspace_order/target
 #!将现有的jar备份后，将新的jar包替换
-file="/deploy/order-0.0.1-SNAPSHOT.jar"
+file="/root/deploy/order-0.0.1-SNAPSHOT.jar"
 if [ -f "$file" ]
 then
-mv /deploy/order-0.0.1-SNAPSHOT.jar /deploy_backup/order-0.0.1-SNAPSHOT.jar.`date +%Y%m%d%H%M%S`
+mv /root/deploy/order-0.0.1-SNAPSHOT.jar /root/deploy_backup/order-0.0.1-SNAPSHOT.jar.`date +%Y%m%d%H%M%S`
 fi
-cp ~/.jenkins/workspace/idea_workspace_order/target/order-0.0.1-SNAPSHOT.jar /deploy
+cp /root/.jenkins/workspace/idea_workspace_order/target/order-0.0.1-SNAPSHOT.jar /root/deploy
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 ehco "授予当前用户权限"
-chmod 777 /deploy/order-0.0.1-SNAPSHOT.jar
+chmod 777 /root/deploy/order-0.0.1-SNAPSHOT.jar
 echo "执行....."
-cd /deploy
-java -jar cmp-0.0.1-SNAPSHOT.jar
+cd /root/deploy
+java -jar order-0.0.1-SNAPSHOT.jar
 echo "**********************cmp on  jenkins started*************************"
